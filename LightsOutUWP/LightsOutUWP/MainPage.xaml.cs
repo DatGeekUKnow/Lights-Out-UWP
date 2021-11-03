f477ae598965e236b2f7d3fe01cde27b3dafd916
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2021-11-03 13:49:38 -0500

    began copying stuff over

--- LightsOutUWP/LightsOutUWP/MainPage.xaml.cs
+++ LightsOutUWP/LightsOutUWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,44 @@ namespace LightsOutUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private LightsOutGame game;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            game = new LightsOutGame();
+
+            CreateGrid();
+            DrawGrid();
+            
+        }
+
+        private void CreateGrid()
+        {
+            boardCanvas.Children.Clear();
+            int rectSize = (int)boardCanvas.Width / game.GridSize;
+            // Create rectangles for grid
+            for (int r = 0; r < game.GridSize; r++)
+            {
+                for (int c = 0; c < game.GridSize; c++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = new SolidColorBrush(Colors.White);
+                    rect.Width = rectSize + 1;
+                    rect.Height = rect.Width + 1;
+                    rect.Stroke = new SolidColorBrush(Colors.Black);
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    //Register event handler
+                    
+                    // Put the rectangle at the proper location within the canvas
+                    Canvas.SetTop(rect, r * rectSize);
+                    Canvas.SetLeft(rect, c * rectSize);
+                    // Add the new rectangle to the canvas' children
+                    boardCanvas.Children.Add(rect);
+                }
+            }
         }
     }
 }
